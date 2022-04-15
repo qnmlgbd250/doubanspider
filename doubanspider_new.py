b72465a618cf75fa448ae45d4355a44a196d9aea
孙福凯 <zcshiyonghao@163.com>
2022-04-15 11:05:58 +0800

    提交

--- doubanspider_new.py
+++ doubanspider_new.py
@@ -16,15 +16,16 @@ HTML_QUEUE = Queue()
 SID_QUEUE = Queue()
 info_lists = []
 
+
 class DoubanSpider_new(object):
     def __init__(self):
-        self.redis_pool = redis.ConnectionPool(host = '127.0.0.1', port = 6379, db = 1, decode_responses = True)
-        self.redis_conn = redis.Redis(connection_pool = self.redis_pool)
+        self.redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, db=1, decode_responses=True)
+        self.redis_conn = redis.Redis(connection_pool=self.redis_pool)
 
         self.url = 'https://movie.douban.com/people/216412178/collect'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
-            'cookie': 'll="118282"; bid=3sEC5kpf9bc; douban-fav-remind=1; __utmz=30149280.1649592275.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; _ga=GA1.2.1441050839.1649592004; dbcl2="216412178:3610rHJofMI"; push_noty_num=0; push_doumail_num=0; __utmv=30149280.21641; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1649680620%2C%22https%3A%2F%2Fwww.baidu.com%2Fs%3Fie%3DUTF-8%26wd%3D%25E8%25B1%2586%25E7%2593%25A3%22%5D; _pk_id.100001.8cb4=cf05602ccd82cc23.1647846958.5.1649681840.1649594873.; ck=CN1H; ap_v=0,6.0; __utmc=30149280; __utma=30149280.1441050839.1649592004.1649765916.1649767964.5; __utmt=1; __utmb=30149280.6.10.1649767964'
+            'cookie': 'bid=jWW9x2iQ-Cs; douban-fav-remind=1; ll="118282"; push_noty_num=0; push_doumail_num=0; _vwo_uuid_v2=D3717E91728222C73EB143CFBEEEF5340|ccd202c5cdc46b612bd4f89715292687; dbcl2="216412178:3610rHJofMI"; __utmv=30149280.21641; __utma=30149280.957401677.1649945701.1649945701.1649983377.2; __utmz=30149280.1649983377.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utma=223695111.620959253.1649983377.1649983377.1649983377.1; __utmz=223695111.1649983377.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; ct=y; ck=CN1H; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1649988290%2C%22https%3A%2F%2Fwww.douban.com%2Fpeople%2F216412178%2F%3F_i%3D9988286jWW9x2i%22%5D; _pk_ses.100001.4cf6=*; _pk_id.100001.4cf6=6c8d5bdfc0625dba.1649638407.7.1649988293.1649985190.'
         }
 
     def get_every_page_url(self):
@@ -38,7 +39,7 @@ class DoubanSpider_new(object):
                 'mode': 'list',
                 'tags_sort': 'count'
             }
-            response = requests.get(self.url, params = params, headers = self.headers)
+            response = requests.get(self.url, params=params, headers=self.headers)
             if response.status_code == 200:
                 HTML_QUEUE.put(response.text)
                 print('第' + str(i) + '页加入队列成功')
@@ -53,7 +54,7 @@ class DoubanSpider_new(object):
             for sid in sid_list:
                 self.redis_conn.lpush('sid_list', str(sid))
             print(str(sid_list) + '电影sid列表加入队列成功')
-
+        print('当前队列获取总数：' + str(self.redis_conn.llen('sid_list')))
 
     def get_every_page_movie_info(self):
         print('获取每页电影信息线程启动')
@@ -64,11 +65,14 @@ class DoubanSpider_new(object):
             response = requests.get(url, headers=self.headers)
             if response.status_code == 200:
                 movie_name = re.search(r'<span property="v:itemreviewed">(.*?)</span>', response.text).group(1)
-                movie_score = re.search(r'<strong class="ll rating_num" property="v:average">(.*?)</strong>', response.text).group(1)
-                movie_time = re.search(r'<span property="v:initialReleaseDate" content="(.*?)">', response.text).group(1)
+                movie_score = re.search(r'<strong class="ll rating_num" property="v:average">(.*?)</strong>',
+                                        response.text).group(1)
+                movie_time = re.search(r'<span property="v:initialReleaseDate" content="(.*?)">', response.text).group(
+                    1)
                 movie_style = re.findall(r'<span property="v:genre">(.*?)</span>', response.text)
                 movie_director = re.findall(r'<meta property="video:actor" content="(.*?)" />', response.text)
 
+                info_list.append(sid)
                 info_list.append(movie_name)
                 info_list.append(movie_score if movie_score else '暂无评分')
                 info_list.append(movie_time if movie_time else '暂无上映时间')
@@ -81,13 +85,30 @@ class DoubanSpider_new(object):
                 print('保存数据失败')
                 self.redis_conn.lpush('sid_list', str(sid))
                 print(f'{sid}>>重新放入队列')
+                break
+
+    def find_fish(self):
+        with open('douban_movie.csv', 'rt', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            column = [row[0] for row in reader]
+        while True:
+            sid = self.redis_conn.rpop('sid_list')
+            if str(sid) not in column:
+                print(sid + '>>没有找到')
+                break
+            else:
+                continue
+
+
+
+
 
 if __name__ == '__main__':
     douban = DoubanSpider_new()
     douban.get_every_page_url()
 
     thred1_list = []
-    for _ in range(1):
+    for _ in range(10):
         t1 = Thread(target=douban.get_every_page_movie_sid)
         thred1_list.append(t1)
         t1.start()
@@ -95,16 +116,16 @@ if __name__ == '__main__':
     for t1_ in thred1_list:
         t1_.join()
 
-    thred2_list = []
-    for _ in range(20):
-        t2 = Thread(target=douban.get_every_page_movie_info)
-        t2.start()
-        thred2_list.append(t2)
-
-    for t2_ in thred2_list:
-        t2_.join()
-
-    with open('douban_movie.csv', 'a+', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['movie_name', 'movie_score', 'movie_time', 'movie_style', 'movie_director'])
-        writer.writerows(info_lists)
+    # thred2_list = []
+    # for _ in range(25):
+    #     t2 = Thread(target=douban.get_every_page_movie_info)
+    #     t2.start()
+    #     thred2_list.append(t2)
+    #
+    # for t2_ in thred2_list:
+    #     t2_.join()
+    #
+    # with open('douban_movie.csv', 'a+', encoding='utf-8',newline='') as file:
+    #     writer = csv.writer(file)
+    #     # writer.writerow(['movie_sid','movie_name', 'movie_score', 'movie_time', 'movie_style', 'movie_director'])
+    #     writer.writerows(info_lists)
--- test.py
+++ test.py
@@ -4,8 +4,18 @@
 # @Email   : zcshiyonghao@163.com
 # @File    : test.py
 # @Software: PyCharm
-from lxml import etree
+# from lxml import etree
+#
+# tree = etree.HTML('./test.html')
+# li_list = tree.xpath('//*[@id="content"]/div[2]/div[1]/div[2]/div')
+# print(li_list)
 
-tree = etree.HTML('./test.html')
-li_list = tree.xpath('//*[@id="content"]/div[2]/div[1]/div[2]/div')
-print(li_list)
+import re
+import csv
+
+sid = '30306570'
+with open('douban_movie.csv','rt',encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    column = [row[0] for row in reader]
+
+    print(column)
